cedc639f448fc0d686d4760305bb80c4b2af6674
Kai <kai@pop-os.localdomain>
2020-10-17 17:22:55 +0800

    Updates

---
+++ precache-manifest.33f4c1ded422f872a3ec92e83a4e54f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030f8d3f58024aa89f730da5420cd1c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2134a9f89306c4d70c",
+    "url": "/static/js/2.9fde2b17.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9fde2b17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131ff8af9e10d4367d17",
+    "url": "/static/js/main.ac0b89b9.chunk.js"
+  },
+  {
+    "revision": "880ed563aebf5d9a8faf",
+    "url": "/static/js/runtime-main.c544f286.js"
+  }
+]);
