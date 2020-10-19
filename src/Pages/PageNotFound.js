fba21b12d24743b307d9b8202c2ea59e7896d54c
Ng Jing Kai <37670295+marshmellochoco@users.noreply.github.com>
2020-10-20 02:42:00 +0800

    Completed Home page
    - Added Navbar component and 2 png images to component folder
    - Added Home JS and CSS
    - Added About JS and CSS

---
+++ src/Pages/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <h2>This page does not exist.</h2>
+            <a href="/#">Click here to go back to the homepage</a>
+        </div>
+    )
+}
+
+
+export default PageNotFound
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Component Import
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import './index.css';
+
+// Pages Import
+import Home from './Pages/Home';
+import About from './Pages/About';
+import PageNotFound from './Pages/PageNotFound';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Router exact path="/"><Home /></Router>
+        <Router exact path="/about"><About /></Router>
+        <Router path="*"><PageNotFound /></Router>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// TODO: add a component (PageNotFound) for 404 pages in hash routes
